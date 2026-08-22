a857b2beae42362509576d31394813cac3df3d88
ursan <ursan.ghe@gmail.com>
2026-08-22 19:55:12 +0200

    feat(noctalia): transparent bar and dark theme settings

--- modules/noctalia.nix
+++ modules/noctalia.nix
@@ -7,19 +7,54 @@
         enable = true;
         systemd.enable = true;
         settings = {
-          shell.setup_wizard_enabled = false;
+
           wallpaper.enabled = false;
           dock.enabled = false;
+
+	  theme = {
+	    mode = "dark";
+	    templates = {
+	      enable_builtin_templates = false;
+	      enable_community_templates = false;
+	    };
+	  };
+
+	  shell = {
+	    setup_wizard_enabled = false;
+	    panel = {
+	      transparency_mode = "solid";
+	      borders = false;
+   	      shadow = false;
+	    };
+	  }; 
+
           bar = {
             order = [ "main" ];
             main = {
               enabled = true;
               position = "top";
+	      auto_hide = false;
+	      reserve_space = true;
+	      layer = "top";
+	      thickness = 30;
+	      padding = 4;
+	      widget_spacing = 7;
+	      background_opacity = 0.0;
+	      border_width = 0.0;
+	      shadow = false;
+	      capsule = false;
+	      radius = 12;
+	      margin_ends = 9;
+	      margin_edge = 4;
+	      scale = 1.0;
+	      font_family = "JetBrainsMono Nerd Font";
+	      font_weight = "semibold";
               start = [ "workspaces" ];
               center = [ "active_window" ];
               end = [ "tray" "volume" "network" "battery" "clock" ];
             };
           };
+
           lockscreen = {
 	    enabled = true;
 	    blurred_desktop = true;
@@ -30,21 +65,25 @@
 	  idle = {
 	    pre_action_fade_seconds = 1.0;
 	    behavior = {
+
               lock = {
 	        timeout = 3600;
 		action = "lock";
 		enabled = true;
 	      };
+
               "screen-off" = {
  		timeout = 0;
 		action = "screen_off";
 		enabled = false;
     	      };
+
               suspend = {
 	        timeout = 0;
 		action = "lock_and_suspend";
 		enabled = true;
 	      };
+
             };
           };  
 	};   
