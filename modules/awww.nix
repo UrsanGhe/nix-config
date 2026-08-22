1897bc30c1a3c417ea66b292a844f999979b7e08
ursan <ursan.ghe@gmail.com>
2026-08-22 19:53:59 +0200

    feat(awww): add user systemd wallpaper daemon

---
+++ modules/awww.nix
@@ -0,0 +1,34 @@
+{ inputs, pkgs, ... }:
+let
+  awww = inputs.awww.packages.${pkgs.stdenv.hostPlatform.system}.awww;
+  wallpaper = ../assets/wallpaper.jpg;
+in
+{
+  environment.systemPackages = [
+    awww
+  ];
+
+
+  home-manager.sharedModules = [
+    {
+      systemd.user.services.awww-daemon = {
+        description = "An Answer to your Wayland Wallpaper Woes";
+        documentation = [ "man: awww-daemon(1)" ];
+	
+
+	partOf = [ "graphical-session.target" ];
+	after = [ "graphical-session.target" ];
+	requisite = [ "graphical-session.target" ];
+        wantedBy = [ "graphical-session.target" ];
+
+	
+	serviceConfig = {
+	  Type = "simple";
+	  ExecStart = "${awww}/bin/awww-daemon";
+	  ExecStartPost = "${awww}/bin/awww img ${wallpaper}";
+	  Restart = "on-failure";
+        };
+      };
+    }
+  ];
+}
