bbe2388c7fc07aad5b9413866e24dab0917ef7ad
ursan <ursan.ghe@gmail.com>
2026-09-07 21:35:30 +0200

    fix(kanshi): fixed kanshi settings

--- modules/kanshi.nix
+++ modules/kanshi.nix
@@ -1,54 +1,50 @@
 { inputs, pkgs, ... }:
 let
-  awww = inputs.awww.packages.${pkgs.stdenv.hostPlatform.system).aww;
+  awww = inputs.awww.packages.${pkgs.stdenv.hostPlatform.system}.awww;
   wallpaper = ../assets/wallpaper.jpg;
 in
 {
-  home-manager.sharedModules = {
+  home-manager.sharedModules = [
     {
       services.kanshi = {
         enable = true;
-	package = pkgs.kanshi;
-	systemdTarget = "graphical-session.target";
-	
-	setings = [
-	  {
-	    profile = {
-	      name = "hdmi-only";
-	      outputs = [
-		{
-   	          criteria = "HDMI-A-1";
-		  status = "enable";
-		  mode = "2560x1440@144";
-		  position = "0,0";
-		  scale = 1.0;
-		}
-	      ];
-	    };
-	  };
-	}
+        package = pkgs.kanshi;
+        systemdTarget = "graphical-session.target";
 
-
-	{
-	  profile = {
-	    name = "dp-only";
-	    outputs = [
-	      {
-	        criteria = "DP-2";
-		status = "enable"
-		mode = "1920x1080@144";
-		position = "0,0";
-		scale = 1.0;
-	      }
-            ];
-	  };
-        }
-
-	{
-	  profile = {
-	    name = "dual";
-	    outputs = [
-	      {
+        settings = [
+          {
+            profile = {
+              name = "hdmi-only";
+              outputs = [
+                {
+                  criteria = "HDMI-A-1";
+                  status = "enable";
+                  mode = "2560x1440@144";
+                  position = "0,0";
+                  scale = 1.0;
+                }
+              ];
+            };
+          }
+          {
+            profile = {
+              name = "dp-only";
+              outputs = [
+                {
+                  criteria = "DP-2";
+                  status = "enable";
+                  mode = "1920x1080@144";
+                  position = "0,0";
+                  scale = 1.0;
+                }
+              ];
+            };
+          }
+          {
+            profile = {
+              name = "dual";
+              outputs = [
+                {
                   criteria = "HDMI-A-1";
                   status = "enable";
                   mode = "2560x1440@144";
