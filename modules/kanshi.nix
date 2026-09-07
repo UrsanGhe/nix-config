37d2aacc34f22a7110ea1220ecbd0b6185526ce5
ursan <ursan.ghe@gmail.com>
2026-09-07 21:25:16 +0200

    feat(kanshi): added kanshi for display controls

---
+++ modules/kanshi.nix
@@ -0,0 +1,75 @@
+{ inputs, pkgs, ... }:
+let
+  awww = inputs.awww.packages.${pkgs.stdenv.hostPlatform.system).aww;
+  wallpaper = ../assets/wallpaper.jpg;
+in
+{
+  home-manager.sharedModules = {
+    {
+      services.kanshi = {
+        enable = true;
+	package = pkgs.kanshi;
+	systemdTarget = "graphical-session.target";
+	
+	setings = [
+	  {
+	    profile = {
+	      name = "hdmi-only";
+	      outputs = [
+		{
+   	          criteria = "HDMI-A-1";
+		  status = "enable";
+		  mode = "2560x1440@144";
+		  position = "0,0";
+		  scale = 1.0;
+		}
+	      ];
+	    };
+	  };
+	}
+
+
+	{
+	  profile = {
+	    name = "dp-only";
+	    outputs = [
+	      {
+	        criteria = "DP-2";
+		status = "enable"
+		mode = "1920x1080@144";
+		position = "0,0";
+		scale = 1.0;
+	      }
+            ];
+	  };
+        }
+
+	{
+	  profile = {
+	    name = "dual";
+	    outputs = [
+	      {
+                  criteria = "HDMI-A-1";
+                  status = "enable";
+                  mode = "2560x1440@144";
+                  position = "0,0";
+                  scale = 1.0;
+                }
+                {
+                  criteria = "DP-2";
+                  status = "enable";
+                  mode = "1920x1080@144";
+                  position = "2560,0";
+                  scale = 1.0;
+                }
+              ];
+              exec = [
+                "${awww}/bin/awww img ${wallpaper}"
+              ];
+            };
+          }
+        ];
+      };
+    }
+  ];
+}
